05f9c93cd13553e5b0bc786c18d2829dee01a8c4
musamuss <taller.97@mail.ru>
2019-04-02 02:47:50 +0300

    start
    nabrosok

---
+++ Instat/Instat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instat
+//
+//  Created by Муса Дениев on 02/04/2019.
+//  Copyright © 2019 our team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
