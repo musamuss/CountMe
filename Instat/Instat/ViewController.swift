a114e7533e517c56c232f43acecb80633095a387
HaloAssassin973 <cokturqi55@mail.ru>
2019-04-13 16:48:20 +0300

    Add API Wrapper

--- Instat/Instat/ViewController.swift
+++ Instat/Instat/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SwiftInstagram
 
 class ViewController: UIViewController {
 
---
+++ Instat/Pods/Target Support Files/Pods-Instat/Pods-Instat-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Instat : NSObject
+@end
+@implementation PodsDummy_Pods_Instat
+@end
