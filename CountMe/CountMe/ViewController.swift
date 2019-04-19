91804dc5e2332637abdb28140fdd7887c84377c4
HaloAssassin973 <cokturqi55@mail.ru>
2019-04-19 18:50:40 +0300

    SwiftyInsta

--- CountMe/CountMe/ViewController.swift
+++ CountMe/CountMe/ViewController.swift
@@ -7,11 +7,29 @@
 //
 
 import UIKit
+import SwiftyInsta
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let ruserok1 = SessionStorage.create(username: "vwxwgrvu", password: "b9NkHmsf")
+        let userAgent = CustomUserAgent(apiVersion: "79.0.0.0", osName: "iOS", osVersion: "12", osRelease: "1.4", dpi: "458", resolution: "2688x1242", company: "Apple", model: "iPhone11,2", modem: "intel", locale: "en_US", fbCode: "95414346")
+        let urlSession = URLSession(configuration: .default)
+        let handler = try! APIBuilder().createBuilder().setHttpHandler(urlSession: urlSession).setRequestDelay(delay: .default).setUser(user: ruserok1).build()
+        try? handler.login { (result,err) in
+            if result.isSucceeded {
+                try? handler.getUser(username: "Musa Muss",completion: { (result) in
+                    print(result.value?.email)
+                })
+            } else {
+                print("[-] Login failed: \(result.info.error)")
+            }
+        }
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
