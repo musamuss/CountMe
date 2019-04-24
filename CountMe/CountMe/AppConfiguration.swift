cbc878c5c7c03470347e71c9d7acbac8c8ab81ce
musamuss <taller.97@mail.ru>
2019-04-24 16:17:24 +0300

    Update AppConfiguration.swift

--- CountMe/CountMe/AppConfiguration.swift
+++ CountMe/CountMe/AppConfiguration.swift
@@ -31,6 +31,8 @@ class AppConfiguration {
         static let privateAccountMessage = NSLocalizedString("This account is private", comment: "")
         static let reportsCompletedTitle = NSLocalizedString("Reports Completed", comment: "")
         static let reportsCompletedMessage = NSLocalizedString("Please go back to the start screen", comment: "")
+        
+        //стделать тригер на экшон reportsCompletedMessage для перехода на старт скрин
         static let deleteReportsTitle = NSLocalizedString("Delete Reports", comment: "")
         static let deleteReportsMessage = NSLocalizedString("Remove my account and delete all the reports", comment: "")
     }
